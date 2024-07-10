5c8f1392a06725747638c7eb18ef0d3b41f1e347
Jesus Garcia <jgarciajr45@gmail.com>
2024-07-10 09:26:30 -0700

    Ex1 print odds

--- app.js
+++ app.js
@@ -2,6 +2,17 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+function printOdds(count) {
+    if (count < 0) {
+        console.log("Oops! number must be positive");
+        return;
+    }
+    for (let i = 1; i <= count; i++) {
+        if (i % 2 !== 0) {
+            console.log(i);
+        }
+    }
+}//printOdds(10);
 
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
